cf43ca8a65b31551e549139e8031b5657b28bf3e
Noyan <sngermiyanoglu@hotmail.com>
2020-10-19 12:07:01 +0300

    UserControllerTest.java Completed

--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.TestUtils;
 import com.example.demo.controllers.UserController;
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
@@ -10,6 +12,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 
@@ -52,5 +58,66 @@ public class UserControllerTest {
         assertEquals(0, u.getId());
         assertEquals("Username", u.getUsername());
         assertEquals("HashedPassword", u.getPassword());
+    }
+
+    @Test
+    public void findByIdFoundTest(){
+
+        User user = createUser();
+        when(userRepo.findById(1L)).thenReturn(Optional.of(user));
+        ResponseEntity<User> response = userController.findById(1L);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(user, response.getBody());
+    }
+
+    @Test
+    public void findByIdNotFoundTest(){
+        ResponseEntity<User> response = userController.findById(1L);
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void findByUsernameFoundTest(){
+
+        User user = createUser();
+        when(userRepo.findByUsername("Username")).thenReturn(user);
+        ResponseEntity<User> response = userController.findByUserName("Username");
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(user, response.getBody());
+        assertEquals(1L, user.getId());
+        assertEquals("Username", user.getUsername());
+        assertEquals("Password", user.getPassword());
+    }
+
+    @Test
+    public void findByUsernameNotFoundTest(){
+
+        ResponseEntity<User> response = userController.findByUserName("Username");
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    private Cart emptyCart(){
+        Cart cart = new Cart();
+        cart.setId(1L);
+        cart.setUser(null);
+        cart.setItems(new ArrayList<Item>());
+        cart.setTotal(BigDecimal.valueOf(0.0));
+        return cart;
+    }
+
+    private User createUser(){
+        User user = new User();
+        user.setId(1);
+        user.setUsername("Username");
+        user.setPassword("Password");
+        user.setCart(emptyCart());
+
+        return user;
     }
 }
